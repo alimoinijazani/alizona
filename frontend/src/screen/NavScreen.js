f305e1dcbc4df4d0940f72e0a8e72f21a7b2f308
alimoinijazani <Ali_28848>
2022-12-05 10:59:21 +0330

    modify navscreen

--- frontend/src/screen/NavScreen.js
+++ frontend/src/screen/NavScreen.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import NavBar from 'react-bootstrap/NavBar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 export default function NavScreen() {
-  return <NavBar>alizona</NavBar>;
+  return (
+    <header>
+      <NavBar bg="dark" variant="dark">
+        <Container>
+          <LinkContainer to="/">
+            <NavBar.Brand>alizona</NavBar.Brand>
+          </LinkContainer>
+        </Container>
+      </NavBar>
+    </header>
+  );
 }
