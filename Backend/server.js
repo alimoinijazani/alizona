16d5a8e7c5e88dd05ca7a5d39404135937dfec2b
alimoinijazani <Ali_28848>
2023-01-06 11:53:31 +0330

    deploy

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,7 @@ import productRouter from './Routes/productRoutes.js';
 import userRouter from './Routes/userRoutes.js';
 import orderRouter from './Routes/orderRoutes.js';
 import uploadRouter from './Routes/uploadRoutes.js';
+import path from 'path';
 dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -27,6 +28,13 @@ app.use('/api/upload', uploadRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
